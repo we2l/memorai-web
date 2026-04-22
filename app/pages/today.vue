7da4ac979ddf8a34ed8f1dfaacafab71d55a1574
Weslley araujo de sousa leite <weslleyadesousa@gmail.com>
2026-04-22 19:42:27 -0300

    debug: checklist values

--- app/pages/today.vue
+++ app/pages/today.vue
@@ -144,13 +144,15 @@
     </div>
 
     <!-- Activation checklist -->
-    <div class="mt-8">
+    <div v-if="stats" class="mt-8">
+      <!-- debug -->
+      <p class="text-micro text-danger mb-2">DEBUG: cards={{ stats.total_cards }} decks={{ stats.total_decks }} topics={{ topicProgress.length }} reviewed={{ stats.cards_reviewed_today }} streak={{ stats.streak }}</p>
       <UiActivationChecklist
         :has-topics="topicProgress.length > 0"
-        :has-material="(stats?.total_decks ?? 0) > 0"
-        :has-cards="(stats?.total_cards ?? 0) > 0"
-        :has-reviewed="(stats?.cards_reviewed_today ?? 0) > 0 || (stats?.streak ?? 0) > 0"
-        :streak="stats?.streak ?? 0"
+        :has-material="(stats.total_decks ?? 0) > 0"
+        :has-cards="(stats.total_cards ?? 0) > 0"
+        :has-reviewed="(stats.cards_reviewed_today ?? 0) > 0 || (stats.streak ?? 0) > 0"
+        :streak="stats.streak ?? 0"
       />
     </div>
 
